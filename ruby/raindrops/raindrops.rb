9a429cf6e43d2e0ce8a15bbde8c26fab2aa28237
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-10 19:42:04 +0900

    Use map instead of each

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,10 +1,9 @@
 module Raindrops
   PAIRS = [[3, "Pling"], [5, "Plang"], [7, "Plong"]]
   def self.convert(n)
-    answer = ""
-    PAIRS.each do |divisor, word|
-      answer += word if n % divisor == 0
-    end
+    answer = PAIRS.map do |divisor, word|
+       n % divisor == 0 ? word : ""
+    end.join
     answer.empty? ? n.to_s : answer
   end
 end
