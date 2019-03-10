8e2f96d909cc97cd9d4fdef0d7dba9e60c6bdf33
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-10 09:12:27 +0900

    Add raindrops.rb

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,10 @@
+module Raindrops
+  def self.convert(n)
+    answer = ""
+    answer += "Pling" if n % 3 == 0
+    answer += "Plang" if n % 5 == 0
+    answer += "Plong" if n % 7 == 0
+    answer = n.to_s if answer.empty?
+    answer
+  end
+end
