0e913c6ca921f2867a640cc2a042a31e49c94037
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-12 05:42:47 +0900

    Extract to private functions

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -19,15 +19,23 @@ defmodule Roman do
   """
   @spec numerals(pos_integer) :: String.t()
   def numerals(number) do
+    number
+    |> simple_convert
+    |> shorten
+  end
+
+  defp simple_convert(number) do
     {str, _} =
       @roman_letters
       |> Enum.reduce({"", number}, fn roman, {s, n} ->
         {duplicate(roman, rem(n, 10)) <> s, div(n, 10)}
       end)
 
+    str
+  end
+
+  defp shorten(str) do
     @replace_patterns
-    |> Enum.reduce(str, fn {pattern, replacement}, s ->
-      replace(s, pattern, replacement)
-    end)
+    |> Enum.reduce(str, fn {pattern, replacement}, s -> replace(s, pattern, replacement) end)
   end
 end
