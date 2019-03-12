415977804888b283f6266b1044d31d0515c05d4d
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-12 22:43:56 +0900

    Add rotational-cipher.jl

---
+++ julia/rotational-cipher/rotational-cipher.jl
@@ -0,0 +1,14 @@
+function rotate(n::Int, str::Union{AbstractString,AbstractChar})
+    ischar = isa(str, Char)
+    lower = join('a':'z')
+    upper = join('A':'Z')
+    from = lower * upper
+    to = make_str(lower, n) * make_str(upper, n)
+    str |> join |> s -> split(s, "") |>
+    s -> map(s) do s
+        idx = findfirst(s, from)
+        idx !== nothing ? SubString(to, idx) : s
+    end |> join |> s -> ischar ? s[1] : s
+end
+
+make_str(str, n) = SubString(str, n+1) * SubString(str, 1, n)
