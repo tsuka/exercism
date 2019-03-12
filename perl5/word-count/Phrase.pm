f3721782e58414d07ea91a27e45ef58a0bbe2d3f
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-13 07:18:52 +0900

    Add Phrase.pm

---
+++ perl5/word-count/Phrase.pm
@@ -0,0 +1,15 @@
+package Phrase;
+
+use strict;
+use warnings;
+
+sub word_count {
+    my $phrase = shift;
+    $phrase = lc($phrase);
+    my $count = {};
+    while ($phrase =~ /(\w+)/g) {
+        $count->{$1} += 1;
+    }
+    return $count;
+}
+1;
