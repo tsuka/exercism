9e9b4d821800c682875ca9acce7d8114173d7268
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-14 07:15:16 +0900

    Refactor

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,5 +1,5 @@
 class Scrabble
-  SCORE_TABLE = Hash[*[
+  SCORE_TABLE = [
     [ %w(A E I O U L N R S T), 1],
     [ %w(D G),                 2],
     [ %w(B C M P),             3],
@@ -7,9 +7,13 @@ class Scrabble
     [ %w(K),                   5],
     [ %w(J X),                 8],
     [ %w(Q Z),                10],
-  ].flat_map { |letters, score|
-    letters.map {|letter| [letter, score]}
-  }.flatten]
+  ].reduce({}) do |h, pairs|
+      letters, score = pairs
+      letters.each do |letter|
+        h[letter] =  score
+      end
+      h
+    end
   SCORE_TABLE.default = 0
 
   def self.score(word)
@@ -17,10 +21,14 @@ class Scrabble
   end
 
   def initialize(word)
-    @word = word&.upcase
+    @word = word || ""
   end
 
   def score
-    @word && @word.chars.map{|c| SCORE_TABLE[c]}.reduce(:+) || 0
+    letters.map(&SCORE_TABLE).sum
+  end
+
+  def letters
+    @word.upcase.chars
   end
 end
