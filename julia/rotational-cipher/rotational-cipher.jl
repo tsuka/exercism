d5e98eec69ac2c0969116eb0d35e723810ed5b68
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-14 00:50:41 +0900

    Simplify

--- julia/rotational-cipher/rotational-cipher.jl
+++ julia/rotational-cipher/rotational-cipher.jl
@@ -1,20 +1,12 @@
 function rotate(n::Int, c::AbstractChar)
-    if 'a' <= c <= 'z'
-        mod((c - 'a' + n), 26) + 'a'
-    elseif 'A' <= c <= 'Z'
-        mod((c - 'A' + n), 26) + 'A'
-    else
-        c
-    end
+    isletter(c) || return c
+    basechar = isuppercase(c) ? 'A' : 'a'
+    return (c - basechar + n) % 26 + basechar
 end
 
-
-function rotate(n::Int, str::AbstractString)
-    map(i -> rotate(n, str[i]), 1:length(str)) |> join
-end
+rotate(n::Int, str::AbstractString) = map(c -> rotate(n, c), str)
 
 make_str(str, n) = SubString(str, n+1) * SubString(str, 1, n)
-
 
 0:26 |> nums -> map(nums) do num
     macro_name = Symbol("R$(num)_str")
