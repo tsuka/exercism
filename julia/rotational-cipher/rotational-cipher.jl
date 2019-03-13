28efcd6e140706310a436c80b87dc6932ebb1fbe
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-14 01:27:36 +0900

    Remove unnecessary function

--- julia/rotational-cipher/rotational-cipher.jl
+++ julia/rotational-cipher/rotational-cipher.jl
@@ -6,8 +6,6 @@ end
 
 rotate(n::Int, str::AbstractString) = map(c -> rotate(n, c), str)
 
-make_str(str, n) = SubString(str, n+1) * SubString(str, 1, n)
-
 0:26 |> nums -> map(nums) do num
     macro_name = Symbol("R$(num)_str")
     eval(quote
