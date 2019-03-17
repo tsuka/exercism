df42edad2d8b71b5d1fd6727d55457cc350aa6fc
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-18 05:04:12 +0900

    Add difference-of-squares.jl

---
+++ julia/difference-of-squares/difference-of-squares.jl
@@ -0,0 +1,8 @@
+"Sum the squares of the numbers up to the given number"
+square_of_sum(n::Int) = map(n -> n ^ 2, 1:n |> sum)
+
+"Square the sum of the numbers up to the given number"
+sum_of_squares(n::Int) = map(n -> n ^ 2, 1:n) |> sum
+
+"Subtract sum of squares from square of sums"
+difference(n::Int) = square_of_sum(n) - sum_of_squares(n)
