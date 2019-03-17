843235104a563b083099560fe5228f4a41e484ef
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-18 05:07:27 +0900

    Refactor

--- julia/difference-of-squares/difference-of-squares.jl
+++ julia/difference-of-squares/difference-of-squares.jl
@@ -1,5 +1,5 @@
 "Sum the squares of the numbers up to the given number"
-square_of_sum(n::Int) = map(n -> n ^ 2, 1:n |> sum)
+square_of_sum(n::Int) = sum(1:n) ^ 2
 
 "Square the sum of the numbers up to the given number"
 sum_of_squares(n::Int) = map(n -> n ^ 2, 1:n) |> sum
