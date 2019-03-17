48d636a53ef72e416eed00ae5cb385a646ed9092
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-17 22:58:19 +0900

    Refactor

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,12 +1,23 @@
 module Luhn
   def self.valid?(digits)
-    digits = digits.tr(' ', '').reverse
-    return false if digits.length <= 1 || digits !~ /\A\d+\Z/
-
-    digits.chars.map(&:to_i).each_slice(2).map{|a,b=0| a + adjust(b)}.sum % 10 == 0
+    digits = digits.delete(' ').reverse
+    return false if invalid_length?(digits) || contains_invalid_char?(digits)
+    (checksum(digits) % 10).zero?
   end
 
-  def self.adjust(num)
+  def self.invalid_length?(digits)
+    digits.length <= 1
+  end
+
+  def self.contains_invalid_char?(digits)
+    digits !~ /\A\d+\Z/
+  end
+
+  def self.checksum(digits)
+    digits.chars.map(&:to_i).each_slice(2).map{|a,b=0| a + double_bounded(b)}.sum
+  end
+
+  def self.double_bounded(num)
     double = num * 2
     double > 9 ? double - 9 : double
   end
