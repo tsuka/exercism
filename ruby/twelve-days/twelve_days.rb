1c618005ddd4e5fb5e00b5b96b4bf2babf85e31e
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-21 07:03:04 +0900

    Add twelve_days.rb

---
+++ ruby/twelve-days/twelve_days.rb
@@ -0,0 +1,17 @@
+module TwelveDays
+
+
+  DAYS = %w(first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth)
+  ITEMS = ["twelve Drummers Drumming", "eleven Pipers Piping", "ten Lords-a-Leaping",
+    "nine Ladies Dancing", "eight Maids-a-Milking", "seven Swans-a-Swimming",
+    "six Geese-a-Laying", "five Gold Rings", "four Calling Birds", "three French Hens",
+    "two Turtle Doves", "a Partridge in a Pear Tree"]
+
+  def self.items(d)
+    ITEMS.drop(11-d).join(", ").sub(/,( [^,]+)$/) {", and#{$1}"}
+  end
+
+  def self.song
+    12.times.map {|d| "On the #{DAYS[d]} day of Christmas my true love gave to me: #{items(d)}.\n"}.join("\n")
+  end
+end
