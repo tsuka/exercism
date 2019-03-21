33596c90386fc01235871090b0ac515dd47c725c
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-21 12:22:15 +0900

    Use struct

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,31 +1,43 @@
 module Tournament
+  Team = Struct.new(:mp, :w, :d, :l, :p) do
+    def win
+      self.mp += 1
+      self.w += 1
+      self.p += 3
+    end
+
+    def loss
+      self.mp += 1
+      self.l += 1
+    end
+
+    def draw
+      self.mp += 1
+      self.d += 1
+      self.p += 1
+    end
+  end
+
   def self.tally(input)
     header = "Team                           | MP |  W |  D |  L |  P\n"
-    table = Hash.new{|h, k| h[k] = {mp: 0, w: 0, d: 0, l: 0, p: 0}}
+    table = Hash.new{|h, k| h[k] = Team.new(0, 0, 0, 0, 0)}
     input.chomp.each_line(chomp: true) do |line|
       team1, team2, result = line.split(";");
-
-      table[team1][:mp] += 1
-      table[team2][:mp] += 1
       case result
       when "win"
-        table[team1][:w] += 1
-        table[team1][:p] += 3
-        table[team2][:l] += 1
+        table[team1].win
+        table[team2].loss
       when "loss"
-        table[team2][:w] += 1
-        table[team2][:p] += 3
-        table[team1][:l] += 1
+        table[team1].loss
+        table[team2].win
       when "draw"
-        table[team1][:d] +=1
-        table[team2][:d] +=1
-        table[team1][:p] +=1
-        table[team2][:p] +=1
+        table[team1].draw
+        table[team2].draw
       end
     end
 
-    header + table.sort_by{|k, v| [-v[:p], k]}.map do |k,v|
-      sprintf("%s|  %d |  %d |  %d |  %d |  %d\n", k.ljust(31), v[:mp], v[:w], v[:d], v[:l], v[:p])
+    header + table.sort_by{|k, v| [-v.p, k]}.map do |k, v|
+      [k.ljust(30), v.mp, v.w, v.d, v.l, v.p].join(" |  ") + "\n"
     end.join
   end
 end
