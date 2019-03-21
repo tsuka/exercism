eb8ef5602fa6ff480da316ce54d23cfef43ea85d
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-21 20:04:29 +0900

    Add Clock.pm

---
+++ perl5/clock/Clock.pm
@@ -0,0 +1,31 @@
+package Clock;
+use strict;
+use warnings;
+
+use constant MINUTE_PER_DAY => 24 * 60;
+
+sub new {
+  my ($class, $attributes) = @_;
+  my ($hour, $minute) = ($attributes->{hour}, $attributes->{minute});
+  return bless {minute=>($hour*60+$minute) % MINUTE_PER_DAY}, $class;
+}
+
+sub time {
+  my ($self) = @_;
+  my $minute = $self->{minute};
+  return sprintf("%02d:%02d", int($minute / 60), $minute % 60);
+}
+
+sub add_minutes {
+  my ($self, $amount) = @_;
+  $self->{minute} = ($self->{minute} + $amount) % MINUTE_PER_DAY;
+  return $self;
+}
+
+sub subtract_minutes {
+  my ($self, $amount) = @_;
+  $self->{minute} = ($self->{minute} - $amount) % MINUTE_PER_DAY;
+  return $self;
+}
+
+1;
