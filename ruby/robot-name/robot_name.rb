3a49414b30162dd832358cd5d372482c39610dda
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-22 05:15:58 +0900

    Refactor

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -2,22 +2,21 @@ class Robot
 
   attr_accessor :name
 
-  ALPHA = ("A".."Z").to_a
-  NUM = ("0".."9").to_a
-
   def self.forget
     @@pointer = 0
-    @@stocked_name = ALPHA.repeated_permutation(2).to_a.product(
-        NUM.repeated_permutation(3).to_a
-    ).map(&:join).shuffle
+    @@stocked_name = ("AA000".."ZZ999").to_a.shuffle
   end
 
   def initialize
     self.reset
   end
 
+  def next_name
+    self.name = @@stocked_name[@@pointer]
+  end
+
   def reset
-    self.name = @@stocked_name[@@pointer] or raise "Limit exceeded."
-    @@pointer += 1
+     raise "Limit exceeded." unless next_name
+     @@pointer += 1
   end
 end
