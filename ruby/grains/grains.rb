f023731d8c52b9256b83657595ea1585fc7395b1
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-23 05:51:00 +0900

    Refactor

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,10 +1,12 @@
 module Grains
+  SQUARES_RANGE = 1..64
+
   def self.square(n)
-    raise ArgumentError if n < 1 || n > 64
+    raise ArgumentError unless SQUARES_RANGE.cover?(n)
     2 ** (n-1)
   end
 
   def self.total
-    (1..64).sum{|n| square(n)}
+    SQUARES_RANGE.sum{|n| square(n)}
   end
 end
