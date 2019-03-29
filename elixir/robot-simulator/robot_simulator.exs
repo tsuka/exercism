157e4eef36fc67d518f12c1819bd0b4d7c32a249
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-30 07:31:17 +0900

    Add robot_simulator.exs

---
+++ elixir/robot-simulator/robot_simulator.exs
@@ -0,0 +1,84 @@
+defmodule RobotSimulator do
+
+  @directions [:north, :east, :south, :west]
+  @move %{north: {0, 1}, east: {1, 0}, south: {0, -1}, west: {-1, 0}}
+
+
+  @doc """
+  Create a Robot Simulator given an initial direction and position.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec create(direction :: atom, position :: {integer, integer}) :: any
+  def create(direction \\ :north, position \\ {0, 0}) do
+    if direction in @directions do
+      case position do
+        {x, y} when is_integer(x) and is_integer(y) -> %{direction: direction, position: position}
+        _ -> {:error, "invalid position"}
+      end
+    else
+      {:error, "invalid direction"}
+    end
+  end
+
+  @doc """
+  Simulate the robot's movement given a string of instructions.
+
+  Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
+  """
+  @spec simulate(robot :: any, instructions :: String.t()) :: any
+  def simulate(robot, instructions) do
+    try do
+      String.to_charlist(instructions) |> Enum.reduce(robot, fn c, r ->
+        case c do
+          ?R -> turn_right(r)
+          ?L -> turn_left(r)
+          ?A -> advance(r)
+          _ -> throw {:error, "invalid instruction"}
+        end
+      end)
+    catch
+      err -> err
+    end
+  end
+
+  @doc """
+  Return the robot's direction.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec direction(robot :: any) :: atom
+  def direction(robot) do
+    %{direction: direction} = robot
+    direction
+  end
+
+  @doc """
+  Return the robot's position.
+  """
+  @spec position(robot :: any) :: {integer, integer}
+  def position(robot) do
+    %{position: position} = robot
+    position
+  end
+
+  defp advance(robot) do
+    %{direction: direction, position: {x, y}} = robot
+    {xx, yy} = @move[direction]
+    %{direction: direction, position: {x+xx, y+yy}}
+  end
+
+  defp turn_right(robot) do
+    turn(1, robot)
+  end
+
+  defp turn_left(robot) do
+    turn(-1, robot)
+  end
+
+  defp turn(n, robot) do
+    %{direction: direction, position: position} = robot
+    idx = Enum.find_index(@directions, &(&1 == direction))
+    %{direction: Enum.at(@directions, Integer.mod(idx+n, 4)), position: position}
+  end
+end
