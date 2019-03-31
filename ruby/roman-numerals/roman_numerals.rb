3cc580cdfe3186f9c082f087ab8eb2766f400443
Akihito Tsukamoto <tsuka05@gmail.com>
2019-03-31 18:01:58 +0900

    Add roman_numerals.rb

---
+++ ruby/roman-numerals/roman_numerals.rb
@@ -0,0 +1,27 @@
+class Integer
+  ROMAN_TABLE = [
+    {1 => "I", 5 => "V", 10 => "X"},
+    {1 => "X", 5 => "L", 10 => "C"},
+    {1 => "C", 5 => "D", 10 => "M"},
+    {1 => "M"}
+  ]
+
+  def to_roman
+    roman, n = ROMAN_TABLE.reduce(["", self]) do |pair, table|
+      roman, n = pair
+      digit = n % 10
+      str = case digit
+      when 9
+        table[1] + table[10]
+      when 5..8
+        table[5] + (table[1] * (digit - 5))
+      when 4
+        table[1] + table[5]
+      else
+        table[1] * digit
+      end
+      [str + roman, n / 10]
+    end
+    roman
+  end
+end
