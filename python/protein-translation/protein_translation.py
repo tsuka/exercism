4fb7b735328487e7e161558e8151383240041bb9
Akihito Tsukamoto <tsuka05@gmail.com>
2019-05-26 15:44:16 +0900

    Use tuple

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -2,18 +2,18 @@ import re
 
 trans_dict = {}
 trans_list = [
-  [["AUG"], "Methionine"],
-  [["UUU", "UUC"], "Phenylalanine"],
-  [["UUA", "UUG"], "Leucine"],
-  [["UCU", "UCC", "UCA", "UCG"], "Serine"],
-  [["UAU", "UAC"], "Tyrosine"],
-  [["UGU", "UGC"], "Cysteine"],
-  [["UGG"], "Tryptophan"],
-  [["UAA", "UAG", "UGA"], "STOP"],
+  (["AUG"], "Methionine"),
+  (["UUU", "UUC"], "Phenylalanine"),
+  (["UUA", "UUG"], "Leucine"),
+  (["UCU", "UCC", "UCA", "UCG"], "Serine"),
+  (["UAU", "UAC"], "Tyrosine"),
+  (["UGU", "UGC"], "Cysteine"),
+  (["UGG"], "Tryptophan"),
+  (["UAA", "UAG", "UGA"], "STOP"),
 ]
-for pair in trans_list:
-  for codon in pair[0]:
-    trans_dict[codon] = pair[1]
+for codons, protein in trans_list:
+  for codon in codons:
+    trans_dict[codon] = protein
 
 def proteins(strand):
   result = []
