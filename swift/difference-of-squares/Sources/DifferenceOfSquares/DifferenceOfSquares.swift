e32781e9eae33ae346cd2e7e218c4f3d3faada7d
Akihito Tsukamoto <tsuka05@gmail.com>
2019-12-12 13:51:51 +0900

    Shorten

--- swift/difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
+++ swift/difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
@@ -6,7 +6,7 @@ struct Squares {
   let differenceOfSquares: Decimal
 
   init(_ num: Int) {
-    self.squareOfSum = pow(Decimal((1...num).reduce(0, {acc, n in acc + n})), 2)
+    self.squareOfSum = pow(Decimal((1...num).reduce(0, +)), 2)
     self.sumOfSquares = (1...num).reduce(0.0, {acc, n in acc + pow(Decimal(n), 2)})
     self.differenceOfSquares = self.squareOfSum - self.sumOfSquares
   }
