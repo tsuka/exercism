41cf2bd19aba5146e58791954be283c6dec7af79
Akihito Tsukamoto <tsuka05@gmail.com>
2019-12-12 13:46:45 +0900

    Add DifferenceOfSquares.swift

---
+++ swift/difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct Squares {
+  let squareOfSum: Decimal
+  let sumOfSquares: Decimal
+  let differenceOfSquares: Decimal
+
+  init(_ num: Int) {
+    self.squareOfSum = pow(Decimal((1...num).reduce(0, {acc, n in acc + n})), 2)
+    self.sumOfSquares = (1...num).reduce(0.0, {acc, n in acc + pow(Decimal(n), 2)})
+    self.differenceOfSquares = self.squareOfSum - self.sumOfSquares
+  }
+}
